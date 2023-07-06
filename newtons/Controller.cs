4e2140c37ce47a1f131a7b4f452fad689df5cfa4
THE-OOZ <th.ngyod@gmail.com>
2023-07-07 01:15:28 +0700

    Add files via upload

---
+++ newtons/Controller.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Globalization;
+using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Exam_Gridview
+{
+    public class Movie
+    {
+        public string Name { get; set; }
+        public string ReleaseDate { get; set; }
+        public List<string> Genres { get; set; }
+    }
+
+    public class Cal
+    {
+        public string Expression { get; set; }
+        public string Operand_1 { get; set; }
+
+    }
+
+    class Controller
+    {
+        
+
+        public string fileContent;
+
+        public string cs;
+        public MySqlConnection conn;
+        public MySqlConnection sqlCon()
+        {
+            cs = @"server=localhost;userid=root;password=;database=calculator";
+            conn = new MySqlConnection(cs);
+            return conn;
+
+        }
+
+        public MySqlCommand sqlCmd()
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            return cmd;
+        }
+
+        public void sqlNonQuery(string cs)
+        {
+            Controller ctr = new Controller();
+            var cmd = ctr.sqlCmd();
+            var conn = ctr.sqlCon();
+
+            conn.Open();
+            cmd = conn.CreateCommand();
+            cmd.CommandText = cs;
+            //  Console.WriteLine("CMD  : " + cmd.CommandText.ToString());
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public MySqlDataReader sqlQuery(string cs)
+        {
+            Controller ctr = new Controller();
+            var cmd = ctr.sqlCmd();
+            var conn = ctr.sqlCon();
+
+            conn.Open();
+            cmd = conn.CreateCommand();
+            cmd.CommandText = cs;
+
+            var data = cmd.ExecuteReader();
+            // conn.Close();
+            return data;
+        }
+
+        public MySqlDataAdapter sqlAdt(string cs)
+        {
+            Controller ctr = new Controller();
+            var conn = ctr.sqlCon();
+            MySqlDataAdapter da = new MySqlDataAdapter(cs, conn);
+            return da;
+
+        }
+
+        public string readFile(string src)
+        {
+            string fileName;
+            string filePath;
+            //string fileContent;
+
+            if (src == "LOCAL_FILE")
+            {
+                fileName = "Lab-Result.txt";                                   // --- debug path --- //
+                filePath = Path.Combine(Application.StartupPath, fileName);
+                fileContent = File.ReadAllText(filePath);
+            }
+            else if (src == "SELECT_FILE")
+            {
+                Stream st;
+                OpenFileDialog opf = new OpenFileDialog();
+
+                if (opf.ShowDialog() == DialogResult.OK)
+                {
+
+                    if ((st = opf.OpenFile()) != null)
+                    {
+                        filePath = opf.FileName;
+                        fileContent = File.ReadAllText(filePath);
+                    }
+                }
+            }
+
+            return fileContent;
+        }
+
+    }
+}
---
+++ newtons/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Globalization;
+using System.IO;
+using System.Collections;
+using Dapper;
+
+namespace Exam_Gridview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+        }
+
+        //private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        //{
+        //    MessageBox.Show(dataGridView1.SelectedRows.ToString());
+        //}
+
+        private void button_qry_Click(object sender, EventArgs e)
+        {
+            Controller ctr = new Controller();
+
+            var conn = ctr.sqlCon();
+            var cmd = ctr.sqlCmd();
+            conn.Open();
+            //var da = ctr.sqlAdt("SELECT * FROM `tbl_history`");
+
+            //DataTable dt = new DataTable();
+
+            //da.Fill(dt);
+
+            //dataGridView1.DataSource = dt;
+            //conn.Close();
+
+            var sql = "SELECT * FROM `tbl_history`";
+
+            // Dapper 
+            var history = conn.Query<Cal>(sql).ToList();
+
+            
+
+            MessageBox.Show($"{ history[1].Expression} ---- {history[1].Operand_1}");
+
+            foreach (var data in history)
+            {
+                Console.WriteLine(data.Expression + "====" + data.Operand_1);
+            }
+
+
+
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0) {
+                MessageBox.Show(dataGridView1.SelectedRows[0].Index.ToString());
+                
+            }
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if(e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                MessageBox.Show(row.Cells[e.ColumnIndex].Value.ToString());
+
+            }
+        }
+
+        private void button_upload_Click(object sender, EventArgs e)
+        {
+            //Controller ctr = new Controller();
+
+            //var fileContent = ctr.readFile("SELECT_FILE");
+
+            //Console.Write(fileContent);
+
+
+            string json = @"[
+                  {
+	                'Name': 'Bad Boys',
+	                'ReleaseDate': '1995-4-7T00:00:00',
+	                'Genres': [
+	                  'Action',
+	                  'Comedy'
+	                ]
+                  },
+                  {
+                  'Name': 'Baby Boys',
+                  'ReleaseDate': '1995-5-7T00:00:00',
+                  'Genres': [
+                    'Comedy',
+	                'funny'
+                  ]
+                }
+                ]";
+
+          
+            ArrayList  arrayM = JsonConvert.DeserializeObject<ArrayList>(json);
+            
+
+            foreach(var data in arrayM)
+            {
+                Movie m = JsonConvert.DeserializeObject<Movie>(data.ToString());
+                MessageBox.Show(m.Genres[1]);
+            }
+      
+           
+        }
+    }
+}
